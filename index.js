6010b57702a36764fb75880dbff6ce4e66cf8d2e
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-18 09:40:38 +0300

    working with loops

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names) {
+    let newcard = [];
+    for (let i = 0; i < names.length; i++) {
+      newcard.push( `Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      //debugger;
+    }
+    return newcard;
+  }
+
+function countDown(n){
+    while(n >= 0) {
+        console.log(n--);
+      }
+} 
+
+//countDown(6);
